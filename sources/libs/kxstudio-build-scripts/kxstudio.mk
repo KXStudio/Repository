a528dc0ac2cccf5f77429a7f8df81d258b17fcbb
falkTX <falktx@falktx.com>
2022-04-17 15:22:25 +0100

    Start placing sources in repo
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ sources/libs/kxstudio-build-scripts/kxstudio.mk
@@ -0,0 +1,23 @@
+#!/usr/bin/make -f
+
+include /usr/share/dpkg/architecture.mk
+
+FLAGS  = -O3 -fPIC -DPIC -fvisibility=hidden -fdata-sections -ffunction-sections -DNDEBUG
+ifeq ($(DEB_HOST_ARCH),armhf)
+FLAGS += -march=armv7ve -mcpu=cortex-a7 -mfloat-abi=hard -mfpu=neon-vfpv4
+else ifeq ($(DEB_HOST_ARCH),arm64)
+FLAGS += -march=armv8-a -mcpu=cortex-a53
+else
+FLAGS += -mtune=generic -msse -msse2 -mfpmath=sse
+endif
+
+ifeq ($(KXSTUDIO_NO_FASTMATH),)
+FLAGS += -ffast-math
+endif
+
+export CFLAGS=$(FLAGS)
+export CXXFLAGS=$(FLAGS) -fvisibility-inlines-hidden
+export CPPFLAGS=
+export LDFLAGS=-Wl,-O1,--as-needed,--no-undefined,--gc-sections,--strip-all
+export PATH:=/opt/kxstudio/bin:$(PATH)
+export PKG_CONFIG_PATH=/opt/kxstudio/lib/pkgconfig

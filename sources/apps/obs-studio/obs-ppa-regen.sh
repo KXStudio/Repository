b0b6c95cb64986396cc8f6bd707ef976c1cf6201
falkTX <falktx@falktx.com>
2023-07-05 15:08:43 +0200

    fixup obs regen
    Signed-off-by: falkTX <falktx@falktx.com>

--- sources/apps/obs-studio/obs-ppa-regen.sh
+++ sources/apps/obs-studio/obs-ppa-regen.sh
@@ -6,6 +6,7 @@ PPA_URL="https://launchpad.net/~obsproject/+archive/ubuntu/obs-studio"
 SUFFIX="0obsproject1"
 VERSION="29.1.2"
 DISTS=("focal" "jammy" "kinetic" "lunar")
+RVER=5
 
 wget -c "${PPA_URL}/+files/obs-studio_29.1.2.orig.tar.gz"
 
@@ -16,11 +17,10 @@ for d in ${DISTS[@]}; do
   tar xf obs-studio_${VERSION}-${SUFFIX}~${d}.debian.tar.xz -C obs-studio
   sed -i "s/Build-Depends:/Build-Depends: carla, carla-dev,/" obs-studio/debian/control
   mkdir -p obs-studio/debian/patches
-  cp *.patch obs-studio/debian/patches
+  cp 1000_carla-plugin.patch obs-studio/debian/patches
   echo 1000_carla-plugin.patch >> obs-studio/debian/patches/series
-  echo 1001_qt5-compat.patch >> obs-studio/debian/patches/series
   pushd obs-studio
-  env DEBEMAIL="falktx@falktx.com" dch -v "${VERSION}-${SUFFIX}+carla3~${d}" -D "${d}" --force-distribution "Add carla plugin host module"
+  env DEBEMAIL="falktx@falktx.com" dch -v "${VERSION}-${SUFFIX}+carla${RVER}~${d}" -D "${d}" --force-distribution "Add carla plugin host module"
   debuild --no-lintian -S -sa -d
   popd
 done
